88c657e5436cae4ee33adf951e237af0d37c3cc8
JVarGRW <jeff012si@gmail.com>
2024-08-21 10:06:46 -0600

    bug button as link instead of link

--- src/components/section_components/FeaturedCategories.jsx
+++ src/components/section_components/FeaturedCategories.jsx
@@ -33,9 +33,12 @@ const FeaturedCategories = ({ categories, title }) => {
                       <h1 className="text-white font-bold w-full text-xl md:text-2xl uppercase mt-auto">
                         {item.title}
                       </h1>
-                      <button className="bg-websecundary text-webprimary md:w-36 rounded-full text-xs lg:text-sm py-3 px-6 lg:px-8 font-bold mt-2 uppercase">
-                        <Link href={item.ref}>Shop Now</Link>
-                      </button>
+                      <Link
+                        href={item.ref}
+                        className="bg-websecundary text-webprimary md:w-36 rounded-full text-xs lg:text-sm py-3 px-6 lg:px-8 font-bold mt-2 uppercase"
+                      >
+                        Shop Now
+                      </Link>
                     </div>
                   </div>
                 ) : (
