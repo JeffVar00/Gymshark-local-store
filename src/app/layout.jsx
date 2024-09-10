3d7be94d9b4545ba7c00adb213901fb0dbe23e14
JVarGRW <jeff012si@gmail.com>
2024-09-09 23:35:24 -0600

    using metadata as const

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,53 +8,24 @@ import Footer from "@/components/section_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export async function generateMetadata(locale) {
-  const descriptions = {
-    en: "Discover high-quality fitness and sports apparel at M&M Clothes CR Official Store. Shop Gymshark and top brands to elevate your performance and style.",
-    es: "Descubre ropa deportiva y de fitness de alta calidad en la tienda oficial de M&M Clothes CR. Compra Gymshark y las mejores marcas para mejorar tu rendimiento y estilo.",
-  };
-
-  const title = {
-    en: "M&M Store CR",
-    es: "Tienda M&M CR",
-  };
-
-  return {
-    title: title[locale] || title["es"],
-    description: descriptions[locale] || descriptions["es"],
-    icons: {
-      icon: "/icon.png",
-      shortcut: "/icon.png",
-      apple: "/icon.png",
-      other: {
-        rel: "apple-touch-icon-precomposed",
-        url: "/icon.png",
-      },
+export const metadata = {
+  title: "M&M Store CR",
+  description:
+    "Descubre ropa deportiva y de fitness de alta calidad en la tienda oficial de M&M Clothes CR. Compra Gymshark y las mejores marcas para mejorar tu rendimiento y estilo.",
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/icon.png",
     },
-  };
-}
+  },
+};
 
-export default async function RootLayout({ children }) {
-  const metadata = await generateMetadata("es");
-
+export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href={metadata.icons.icon} sizes="any" />
-        <link rel="shortcut icon" href={metadata.icons.shortcut} sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          href={metadata.icons.apple}
-          sizes="180x180"
-        />
-        <link
-          rel={metadata.icons.other.rel}
-          href={metadata.icons.other.url}
-          sizes="180x180"
-        />
-      </head>
       <body className={inter.className}>
         <WixClientContextProvider>
           <NavbarModal />
